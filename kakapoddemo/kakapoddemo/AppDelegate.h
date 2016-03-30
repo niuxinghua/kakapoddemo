11046e2ecc492bc9c20a4807b6df0cbb67645205
niuxinghua <970626879@qq.com>
2016-03-30 13:08:58 +0800

    commit

---
+++ kakapoddemo/kakapoddemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kakapoddemo
+//
+//  Created by niuxinghua on 16/3/30.
+//  Copyright © 2016年 com.hjojo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
